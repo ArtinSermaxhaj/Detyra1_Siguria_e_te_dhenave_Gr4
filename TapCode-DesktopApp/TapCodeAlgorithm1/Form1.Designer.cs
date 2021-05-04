1191a3766c41a59a630ec9924a362137ea0afc07
Lind Krasniqi <lind.krasniqi@student.uni-pr.edu>
2021-05-04 14:12:50 +0200

    Implemented Desktop App for Tap code

---
+++ TapCode-DesktopApp/TapCodeAlgorithm1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace TapCodeAlgorithm1
+{
+    partial class TapCode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plainTextLabel = new System.Windows.Forms.Label();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.cipherTextLabel = new System.Windows.Forms.Label();
+            this.txtCipherText = new System.Windows.Forms.TextBox();
+            this.decryptedTextLabel = new System.Windows.Forms.Label();
+            this.txtDecryptedText = new System.Windows.Forms.TextBox();
+            this.btnEnkripto = new System.Windows.Forms.Button();
+            this.btnDekripto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plainTextLabel
+            // 
+            this.plainTextLabel.AutoSize = true;
+            this.plainTextLabel.Location = new System.Drawing.Point(36, 25);
+            this.plainTextLabel.Name = "plainTextLabel";
+            this.plainTextLabel.Size = new System.Drawing.Size(51, 13);
+            this.plainTextLabel.TabIndex = 0;
+            this.plainTextLabel.Text = "PlainText";
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.AccessibleName = "";
+            this.txtPlainText.Location = new System.Drawing.Point(35, 50);
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(351, 20);
+            this.txtPlainText.TabIndex = 1;
+            // 
+            // cipherTextLabel
+            // 
+            this.cipherTextLabel.AutoSize = true;
+            this.cipherTextLabel.Location = new System.Drawing.Point(36, 93);
+            this.cipherTextLabel.Name = "cipherTextLabel";
+            this.cipherTextLabel.Size = new System.Drawing.Size(58, 13);
+            this.cipherTextLabel.TabIndex = 2;
+            this.cipherTextLabel.Text = "CipherText";
+            // 
+            // txtCipherText
+            // 
+            this.txtCipherText.AccessibleName = "";
+            this.txtCipherText.Location = new System.Drawing.Point(35, 125);
+            this.txtCipherText.Name = "txtCipherText";
+            this.txtCipherText.Size = new System.Drawing.Size(427, 20);
+            this.txtCipherText.TabIndex = 3;
+            // 
+            // decryptedTextLabel
+            // 
+            this.decryptedTextLabel.AutoSize = true;
+            this.decryptedTextLabel.Location = new System.Drawing.Point(32, 196);
+            this.decryptedTextLabel.Name = "decryptedTextLabel";
+            this.decryptedTextLabel.Size = new System.Drawing.Size(77, 13);
+            this.decryptedTextLabel.TabIndex = 4;
+            this.decryptedTextLabel.Text = "DecryptedText";
+            this.decryptedTextLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtDecryptedText
+            // 
+            this.txtDecryptedText.AccessibleName = "";
+            this.txtDecryptedText.Location = new System.Drawing.Point(35, 242);
+            this.txtDecryptedText.Name = "txtDecryptedText";
+            this.txtDecryptedText.Size = new System.Drawing.Size(427, 20);
+            this.txtDecryptedText.TabIndex = 5;
+            // 
+            // btnEnkripto
+            // 
+            this.btnEnkripto.AccessibleName = "";
+            this.btnEnkripto.Location = new System.Drawing.Point(560, 121);
+            this.btnEnkripto.Name = "btnEnkripto";
+            this.btnEnkripto.Size = new System.Drawing.Size(123, 21);
+            this.btnEnkripto.TabIndex = 6;
+            this.btnEnkripto.Text = "Enkripto";
+            this.btnEnkripto.UseVisualStyleBackColor = true;
+            this.btnEnkripto.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDekripto
+            // 
+            this.btnDekripto.Location = new System.Drawing.Point(560, 160);
+            this.btnDekripto.Name = "btnDekripto";
+            this.btnDekripto.Size = new System.Drawing.Size(121, 24);
+            this.btnDekripto.TabIndex = 7;
+            this.btnDekripto.Text = "Dekripto";
+            this.btnDekripto.UseVisualStyleBackColor = true;
+            this.btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
+            // 
+            // TapCode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 749);
+            this.Controls.Add(this.btnDekripto);
+            this.Controls.Add(this.btnEnkripto);
+            this.Controls.Add(this.txtDecryptedText);
+            this.Controls.Add(this.decryptedTextLabel);
+            this.Controls.Add(this.txtCipherText);
+            this.Controls.Add(this.cipherTextLabel);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.plainTextLabel);
+            this.Name = "TapCode";
+            this.Text = "TapCode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label plainTextLabel;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.Label cipherTextLabel;
+        private System.Windows.Forms.TextBox txtCipherText;
+        private System.Windows.Forms.Label decryptedTextLabel;
+        private System.Windows.Forms.TextBox txtDecryptedText;
+        private System.Windows.Forms.Button btnEnkripto;
+        private System.Windows.Forms.Button btnDekripto;
+    }
+}
+
---
+++ TapCode-DesktopApp/TapCodeAlgorithm1/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TapCodeAlgorithm1
+{
+    public partial class TapCode : Form
+    {
+        public TapCode()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char[,] matrica = new char[5, 5];
+            MbusheMatricen(matrica);
+            string k = txtPlainText.Text;
+            txtCipherText.Text = Enkripto(matrica, k);
+            
+
+        }
+        private static void MbusheMatricen(char[,] matrica)
+        {
+            int asciiFillestar = 97;
+            for (var x = 0; x < 5; x++)
+            {
+                for (var y = 0; y < 5; y++)
+                {
+                    if (asciiFillestar == 107)
+                    {
+                         asciiFillestar++;
+                         y--;
+                        continue;
+                    }
+                    matrica[x, y] = (char)asciiFillestar;
+                    asciiFillestar++;
+                }
+            }
+        }
+        public static string Enkripto(char[,] matrica, string text)
+        {
+            text = text.ToLower();
+            char[] tekstinchar = text.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+            foreach (var t in tekstinchar)
+            {
+                if (Char.IsLower(t))
+                {
+                    for (var x = 0; x < 5; x++)
+                    {
+                        bool found = false;
+                        for (var y = 0; y < 5; y++)
+                        {
+                            if (t == matrica[x, y])
+                            {
+                                found = true;
+                                for (var i = 0; i <= x; i++)
+                                {
+                                    sb.Append(".");
+                                }
+                                sb.Append(" ");
+                                for (var i = 0; i <= y; i++)
+                                {
+                                    sb.Append(".");
+                                }
+                                sb.Append("  ");
+                                break;
+                            }
+                            else if (t == 'k')
+                            {
+                                found = true;
+                                char l = 'c';
+                                if (l == matrica[x, y])
+                                {
+                                    for (var i = 0; i <= x; i++)
+                                    {
+                                        sb.Append(".");
+                                    }
+                                    sb.Append(" ");
+                                    for (var i = 0; i <= y; i++)
+                                    {
+                                        sb.Append(".");
+                                    }
+                                    sb.Append("  ");
+                                    break;
+                                }
+                            }
+                        }
+                        if (found)
+                            break;
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        private void btnDekripto_Click(object sender, EventArgs e)
+        {
+            char[,] matrica = new char[5, 5];
+            MbusheMatricen(matrica);
+            txtDecryptedText.Text = Dekripto(matrica, txtCipherText.Text);
+        }
+        public static string Dekripto(char[,] matrica, string stringienkriptuar)
+        {
+            string[] stringuShkronjat = stringienkriptuar.Split(new[] {"  "}, StringSplitOptions.None);
+            string[,] karakteret = new string[stringuShkronjat.Length - 1, 2];
+            StringBuilder sb = new StringBuilder();
+            var i = 0;
+            var j = 0;
+            for (var t = 0; t < stringuShkronjat.Length - 1; t++)
+            {
+                string[] k = stringuShkronjat[t].Split(' ');
+                karakteret[i, j] = k[0];
+                karakteret[i, j + 1] = k[1];
+                i++;
+            }
+            for (var kushti = 0; kushti < karakteret.GetLength(0); kushti++)
+            {
+                int x = 0;
+                int y = 0;
+                foreach (char c in karakteret[kushti, 0])
+                {
+                    if (c == '.')
+                    {
+                        x += 1;
+                    }
+                }
+                x -= 1;
+                foreach (char c in karakteret[kushti, 1])
+                {
+                    y += 1;
+                }
+                y -= 1;
+                char k = matrica[x, y];
+                sb.Append(k);
+            }
+            return sb.ToString();
+        }
+    }
+}
